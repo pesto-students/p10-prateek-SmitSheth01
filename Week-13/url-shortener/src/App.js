f01032fb2b5d4566271a945867427a3bbf15ba52
SmitSheth01 <smitsheth01@gmail.com>
2023-07-18 21:28:22 +0530

    Basic html framework with input box and button

---
+++ Week-13/url-shortener/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import InputShortener from './InputShortener';
+import BackgroundAnimate from './BackgroundAnimate';
+
+function App() {
+  return (
+    <div className="container">
+      <InputShortener />
+      <BackgroundAnimate />
+    </div>
+  );
+}
+
+export default App;
