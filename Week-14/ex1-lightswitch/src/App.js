988dcb72293635beb17c9ef73c685be9d8c6698b
SmitSheth01 <smitsheth01@gmail.com>
2023-09-03 19:11:47 +0530

    Basic App for Exercise lightswitch using react

---
+++ Week-14/ex1-lightswitch/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Peer1 from './components/Peer1';
+
+function App() {
+  return (
+    <div className="App">
+      <Peer1 />
+    </div>
+  );
+}
+
+export default App;
